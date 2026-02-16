00fc39b3c1d74bc980d7e9b33ac1653535cafb85
CrystianComY <crystianfernando059@gmail.com>
2026-02-16 00:51:09 -0400

    added viscosity and basic code refactoring, also a demo gif :)

--- README.md
+++ README.md
@@ -3,14 +3,20 @@
 A real-time GPU-based particle simulation built with Taichi in Python.
 Includes gravity, particle collisions, and boundary constraints.
 
+## Demo
+
+![Fluid Simulation](demo.gif)
+
 ## Features
 
 - Pairwise spring-based collision model
 - Real-time visualization
 - Vulkan backend
+- Added Cohesion and repulsion forces
+- Added Viscosity
 
 ## Next Steps
 
-- Add cohesion forces
+- Fix infinite energy glitch
 - Add density-based model (SPH)
 - Optimize with spatial hashing
--- fluid-sim.py
+++ fluid-sim.py
@@ -3,21 +3,74 @@ import taichi.math as tm
 
 ti.init(arch=ti.vulkan, debug=True)
 
-particles = 1000
+particles = 2000
 
 # basic forces 
 n = particles
-dt = 0.005
+dt = 0.002
 gravity = tm.vec2(0, -9.8)
-energyLoss = -0.8
+energy_loss = -0.4
 radius = 0.02
-stiffness = 50
+stiffness = 80
+cohesion_strength = 1.2
+viscosity = 1.4
 
 # basic vectors
 pos = ti.Vector.field(2, dtype=float, shape=n)
 vel = ti.Vector.field(2, dtype=float, shape=n)
 force = ti.Vector.field(2, dtype=float, shape=n)
 mass = ti.field(dtype=float, shape=n)
+
+@ti.func
+def reset_forces(i):
+    force[i] = mass[i] * gravity
+
+@ti.func
+def particle_collision(i, j):
+    vdiff = pos[i] - pos[j]
+    distance = vdiff.norm()
+    
+    if distance > 1e-5:
+        
+        normal = vdiff / distance
+        # repulsion
+        collision_radius = 2 * radius
+        if distance < collision_radius:
+            penetration = collision_radius - distance
+            F_rep = stiffness * penetration * normal
+            force[i] += F_rep
+            force[j] -= F_rep
+
+        #cohesion
+        cohesion_radius = 3 * radius
+        if distance < cohesion_radius:
+            q = distance / cohesion_radius
+            F_coh = -cohesion_strength * (1 - q) * normal
+            force[i] += F_coh
+            force[j] -= F_coh
+            
+        # viscosity
+        if distance < cohesion_radius:
+            vel_diff = vel[i] - vel[j]
+            F_visc = -viscosity * vel_diff
+            force[i] += F_visc
+            force[j] -= F_visc
+
+@ti.func
+def border_collision(i):
+    if pos[i].x < radius:
+        pos[i].x = radius
+        vel[i].x *= energy_loss
+    if pos[i].x > 1 - radius:
+        pos[i].x = 1 - radius
+        vel[i].x *= energy_loss
+
+    if pos[i].y < radius:
+        pos[i].y = radius
+        vel[i].y *= energy_loss
+    if pos[i].y > 1 - radius:
+        pos[i].y = 1 - radius
+        vel[i].y *= energy_loss
 
 # initialize particles
 @ti.kernel
@@ -32,48 +85,28 @@ def init():
 def update():
     # reset forces and apply gravity
     for i in range(n):
-        force[i] = mass[i] * gravity
-
+        reset_forces(i)
 
     # particle collision
     for i in range(n):
         for j in range(i + 1, n):
-            vdiff = pos[i] - pos[j]
-            distance = vdiff.norm()
-            
-            if distance > 1e-5 and distance < radius:
-                normal = vdiff / distance
-                penetration = radius - distance
-                F = stiffness * penetration * normal
-                force[i] += F
-                force[j] -= F
-                    
+            particle_collision(i,j)
+
     #integration
     for i in range(n):
         vel[i] += dt * force[i] / mass[i]
         pos[i] += dt * vel[i]
+        vel[i] *= 0.99
         
-        
-        # colision border 
-        if pos[i].x < 0:
-            pos[i].x = 0
-            vel[i].x *= energyLoss
-        if pos[i].x > 1:
-            pos[i].x = 1
-            vel[i].x *= energyLoss
-        if pos[i].y < 0:
-            pos[i].y = 0
-            vel[i].y *= energyLoss
-        if pos[i].y > 1:
-            pos[i].y = 1
-            vel[i].y *= energyLoss
+        # colision with border 
+        border_collision(i)
 
 
-gui = ti.GUI("Fluid Sim", res=(900, 600))
+gui = ti.GUI("Fluid Sim", res=(600, 300))
 
 init()
 
 while gui.running:
     update()
-    gui.circles(pos.to_numpy(), radius=3)
+    gui.circles(pos.to_numpy(), radius=2)
     gui.show()
